aa22bc50b107aa270579e0e94fafff024be019ce
Rachelgill00 <892335244@qq.com>
2024-07-28 23:28:54 +0900

    2024_07_28_23:28

--- t14_factory/model/student.go
+++ t14_factory/model/student.go
@@ -1,13 +1,26 @@
 package model
 
+import "fmt"
+
 type student struct {
 	name  string
 	score float64
 }
 
+func (s *student) String() string {
+	return fmt.Sprintf("{name: %v, score: %v}\n", s.name, s.score)
+}
 func NewStudent(n string, s float64) *student {
 	return &student{
 		name:  n,
 		score: s,
 	}
+}
+
+func (s *student) GetName() string {
+	return s.name
+}
+
+func (s *student) GetScore() float64 {
+	return s.score
 }
